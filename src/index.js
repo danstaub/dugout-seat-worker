2f9eff52f6ba383502ff267dd650eeb209640ff7
Dan Staub <80787134+danstaub@users.noreply.github.com>
2026-03-10 10:59:22 -0400

    Update AIRTABLE_TOKEN to a shorter value

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const AIRTABLE_TOKEN = "pat9AL6u8CtLWETd6.b33c842bf09ea31161bf376759838ed2cb307734d769dcd82c946d3a6a38a0e1";
+const AIRTABLE_TOKEN = "pat9AL6u8CtLWETd6";
 const AIRTABLE_BASE = "https://api.airtable.com/v0/appgEacIYT6IJyCuZ/Dugout%20Backend";
 
 export default {
